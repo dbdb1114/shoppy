cc894210b84201863e4b0b2ae6954ffacce2b6c8
yujeonghyeon <dbdb1114@naver.com>
2023-08-18 15:47:56 +0900

    admin과 user 분리 및 헤더 변경

--- src/routes/user/Main.jsx
+++ src/routes/user/Main.jsx
@@ -1,9 +1,29 @@
+import { useQuery } from "@tanstack/react-query"
+import { useApiContext } from "../../context/ApiContext";
+import ProductsCard from "../../component/ProductsCard";
 
 export default function Main () {
-
+    
+    const {api} = useApiContext();
+    const {isLoading, error, data : mainProductsList}
+        = useQuery(['mainProducts'],()=> api.productList(true));
+    
+    console.log(mainProductsList)
+    console.log(123)
     return(
-        <div>
-            <h1>메인입니당</h1>
+        <div className="bg-[url('../public/image/mainBanner.jpg')] h-96 bg-center ">
+            <div className="w-full h-full bg-black bg-opacity-30 flex flex-col justify-center items-center text-white">
+                <p className="text-5xl">Shop With Us</p>
+                <p className="text-2xl">Best Products, High Quality</p>
+            </div>
+            <p className="text-mainColor font-bold text-center text-4xl pb-5 my-10 border-b-rose-400 border-b-2">Best Product</p>
+            <div className="flex flex-row w-full gap-5 mt-10">
+                {isLoading ?'':''}
+                {error ? '' : ''}
+                {mainProductsList && mainProductsList.map((item,idx)=>{
+                    return <ProductsCard item={item}/>
+                }) }
+            </div>
         </div>
     )
 }
