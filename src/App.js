5767d751a2ff3de864f9d659ca7c4edf16260b17
yujeonghyeon <dbdb1114@naver.com>
2023-08-18 15:48:23 +0900

    라우팅 처리 변경 ( App => Index로 )

--- src/App.js
+++ src/App.js
@@ -1,70 +1,22 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css';
-import Main from './routes/user/Main';
-import ProductDetail from './routes/user/ProductDetail';
-import Login from './routes/Login';
-import Cart from './routes/user/Cart';
-import AdminMain from './routes/admin/AdminMain';
-import { Children } from 'react';
-import Header from './routes/user/Header';
-import ProductList from './routes/user/ProductList';
-import AdminHeader from './routes/admin/AdminHeader';
-import ProductSubmit from './routes/admin/ProductSubmit';
-import ProductUpdate from './routes/admin/ProductUpdate';
-import AdminProductList from './routes/admin/AdminProductList';
 
-const router = createBrowserRouter([
-  {
-    element: <Header/>,
-    children: [
-      {
-        path: "/",
-        element: <Main/>
-      },
-      {
-        path: "/product/list",
-        element: <ProductList/>
-      },
-      {
-        path: "/product/detail/:productId",
-        element: <ProductDetail/>
-      },
-      {
-        path: "/cart",
-        element: <Cart/>
-      },
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login/>
-  },
-  {
-    element: <AdminHeader/>,
-    children: [
-      {
-        path: '/admin/main',
-        element: <AdminMain/>
-      },
-      {
-        path: '/admin/product',
-        element: <AdminProductList/>
-      },
-      {
-        path: '/admin/product/add',
-        element: <ProductSubmit/>
-      },
-      {
-        path: '/admin/product/info',
-        element: <ProductUpdate/>
-      }
-    ]
-  }
-])
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './component/header/Header';
+import ApiContextProvider from './context/ApiContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-      <RouterProvider router={router}/>
+      <>
+        <Header/>
+        <QueryClientProvider client={queryClient}>
+          <ApiContextProvider>
+            <Outlet/>
+          </ApiContextProvider>
+        </QueryClientProvider>
+      </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './routes/user/Main';
+import ProductDetail from './routes/user/ProductDetail';
+import Login from './routes/Login';
+import Cart from './routes/user/Cart';
+import AdminMain from './routes/admin/AdminMain';
+import ProductList from './routes/user/ProductList';
+import AdminHeader from './component/header/AdminHeader';
+import ProductSubmit from './routes/admin/ProductSubmit';
+import ProductUpdate from './routes/admin/ProductUpdate';
+import AdminProductList from './routes/admin/AdminProductList';
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Main/>
+      },
+      {
+        path: "/product/list",
+        element: <ProductList/>
+      },
+      {
+        path: "/product/detail/:productId",
+        element: <ProductDetail/>
+      },
+      {
+        path: "/cart",
+        element: <Cart/>
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    element: <AdminHeader/>,
+    children: [
+      {
+        path: '/admin/main',
+        element: <AdminMain/>
+      },
+      {
+        path: '/admin/product',
+        element: <AdminProductList/>
+      },
+      {
+        path: '/admin/product/add',
+        element: <ProductSubmit/>
+      },
+      {
+        path: '/admin/product/info',
+        element: <ProductUpdate/>
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
