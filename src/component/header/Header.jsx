79bddeddf668903ccf0300c9105467cc005a8730
yujeonghyeon <dbdb1114@naver.com>
2023-08-20 15:28:29 +0900

    헤더 반응형 작업

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -5,18 +5,18 @@ import {AiOutlineShoppingCart} from "react-icons/ai";
 export default function Header () {
 
     return(
-        <div>
+        <header>
             <div className="flex h-20 justify-between">
                 <Link to='/' className="flex items-center">
-                    <img src="./image/logo.png" alt="logo" className="inline-block w-16 h-16" />
-                    <span className="text-5xl font-bold text-mainColor">Shoppy</span>
+                    <span alt="logo" className="inline-block w-14 h-14 md:w-16 md:h-16 bg-[url('../public/image/logo.png')] bg-cover" />
+                    <span className="text-3xl md:text-5xl font-bold text-mainColor">Shoppy</span>
                 </Link>
                 <div className="flex items-center gap-2 pt-2">
-                    <span>products</span>
+                    <span>product</span>
                     <AiOutlineShoppingCart color="#f96263" size={33}/>
-                    <button className="ml-5 bg-mainColor h-10 w-20 text-white font-bold text-base">Login</button>
+                    <button className="ml-5 bg-mainColor h-10 w-20 text-white font-bold text-base mr-3">Login</button>
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
